26e4b4d95be4792d5711180d585479804187d507
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2018-11-22 16:21:41 +0100

    fixes

--- modules/node/outputs.tf
+++ modules/node/outputs.tf
@@ -13,11 +13,6 @@ output "prepare_nodes_id_list" {
   value       = ["${null_resource.prepare_nodes.*.id}"]
 }
 
-output "allowed_ingress_id_list" {
-  description = "Allowed ingress resource ID list"
-  value       = ["${module.allowed_ingress.rule_id_list}"]
-}
-
 output "associate_floating_ip_id_list" {
   description = "Associate floating IP resource ID list"
   value       = ["${openstack_compute_floatingip_associate_v2.associate_floating_ip.*.id}"]
