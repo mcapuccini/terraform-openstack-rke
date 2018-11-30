b5ef98e57e2d8fbaa68b43dfe198d487cde486e2
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2018-11-30 16:13:57 +0100

    bad_idea

--- modules/acme/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "private_key_pem" {
-  description = "The certificate's private key in PEM format"
-  value = "${acme_certificate.certificate.private_key_pem}"
-}
-
-output "certificate_pem" {
-  description = "The certificate in PEM format"
-  value = "${acme_certificate.certificate.certificate_pem}"
-}
