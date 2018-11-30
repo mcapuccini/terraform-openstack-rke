a1de8582acb95f899b12c7a20a314c40c9aace68
Marco Capuccini <marco.capuccini@farmbio.uu.se>
2018-11-30 12:29:11 +0100

    Formatting

--- modules/rke/main.tf
+++ modules/rke/main.tf
@@ -65,6 +65,7 @@ provider "helm" {
   service_account = "tiller"
   namespace       = "kube-system"
   install_tiller  = false
+
   kubernetes {
     host                   = "${local.api_access}"
     client_certificate     = "${rke_cluster.cluster.client_cert}"
@@ -81,27 +82,32 @@ resource "kubernetes_service_account" "tiller" {
 }
 
 resource "kubernetes_cluster_role_binding" "tiller" {
-    depends_on = ["kubernetes_service_account.tiller"]
-    metadata {
-        name = "tiller"
-    }
-    role_ref {
-        api_group = "rbac.authorization.k8s.io"
-        kind = "ClusterRole"
-        name = "cluster-admin"
-    }
-    subject {
-        kind = "User"
-        name = "system:serviceaccount:kube-system:tiller"
-    }
+  depends_on = ["kubernetes_service_account.tiller"]
+
+  metadata {
+    name = "tiller"
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "cluster-admin"
+  }
+
+  subject {
+    kind = "User"
+    name = "system:serviceaccount:kube-system:tiller"
+  }
 }
 
 resource null_resource "tiller" {
   depends_on = ["kubernetes_cluster_role_binding.tiller"]
+
   provisioner "local-exec" {
     environment {
       KUBECONFIG = "${path.root}/kube_config_cluster.yml"
     }
+
     command = "helm init --service-account tiller --wait"
   }
 }
